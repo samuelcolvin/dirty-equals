aa16b1d62d6492d79e87b39516207deb38e60f97
Samuel Colvin <s@muelcolvin.com>
2022-02-25 13:55:17 +0000

    tweaking mkdocs plugins

--- docs/plugins.py
+++ docs/plugins.py
@@ -14,9 +14,13 @@ except ImportError:
 logger = logging.getLogger('mkdocs.test_examples')
 
 
-def test_examples(config):
+def on_pre_build(config: Config):
+    test_examples()
+
+
+def test_examples():
     """
-    Plug called by mkdocs-simple-hooks to run the examples tests.
+    Run the examples tests.
     """
     if not pytest:
         logger.info('pytest not installed, skipping examples tests')
@@ -27,9 +31,35 @@ def test_examples(config):
             logger.warning('examples tests failed')
 
 
+def on_files(files: Files, config: Config) -> Files:
+    return remove_files(files)
+
+
+def remove_files(files: Files) -> Files:
+    to_remove = []
+    for file in files:
+        if file.src_path in {'plugins.py', 'requirements.txt'}:
+            to_remove.append(file)
+        elif file.src_path.startswith('__pycache__/'):
+            to_remove.append(file)
+
+    logger.debug('removing files: %s', [f.src_path for f in to_remove])
+    for f in to_remove:
+        files.remove(f)
+
+    return files
+
+
 def on_page_markdown(markdown: str, page: Page, config: Config, files: Files) -> str:
-    markdown = reinstate_code_title(markdown)
+    markdown = reinstate_code_titles(markdown)
     return add_version(markdown, page)
+
+
+def reinstate_code_titles(markdown: str) -> str:
+    """
+    Fix titles in code blocks, see https://youtrack.jetbrains.com/issue/PY-53246.
+    """
+    return re.sub(r'^(```py)\s*\ntitle=', r'\1 title=', markdown, flags=re.M)
 
 
 def add_version(markdown: str, page: Page) -> str:
@@ -42,10 +72,3 @@ def add_version(markdown: str, page: Page) -> str:
             version_str = 'Documentation for development version'
         markdown = re.sub(r'{{ *version *}}', version_str, markdown)
     return markdown
-
-
-def reinstate_code_title(markdown: str) -> str:
-    """
-    Fix titles in code blocks, see https://youtrack.jetbrains.com/issue/PY-53246.
-    """
-    return re.sub(r'^(```py)\s*\ntitle=', r'\1 title=', markdown, flags=re.M)
