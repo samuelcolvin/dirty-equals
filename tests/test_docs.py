249435b0abfb5954da1dbcddfbb133fad1f5c44e
Alex Hall <alex.mojaki@gmail.com>
2023-09-16 16:03:20 +0200

    Pass absolute paths to find_examples so that tests don't rely on cwd This makes it easier to run the tests in my IDE

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,11 +1,19 @@
 import platform
+from pathlib import Path
 
 import pytest
 from pytest_examples import CodeExample, EvalExample, find_examples
 
+root_dir = Path(__file__).parent.parent
+
+examples = find_examples(
+    str(root_dir / 'dirty_equals'),
+    str(root_dir / 'docs'),
+)
+
 
 @pytest.mark.skipif(platform.python_implementation() == 'PyPy', reason='PyPy does not allow metaclass dunder methods')
-@pytest.mark.parametrize('example', find_examples('dirty_equals', 'docs'), ids=str)
+@pytest.mark.parametrize('example', examples, ids=str)
 def test_docstrings(example: CodeExample, eval_example: EvalExample):
     prefix_settings = example.prefix_settings()
     # E711 and E712 refer to `== True` and `== None` and need to be ignored
